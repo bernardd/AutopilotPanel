4513d4a3b256d624d8ffbcf4612ac4ed1af12894
Bernard Duggan <bernard@guarana.org>
2020-12-27 22:31:49 +1100

    WIP

--- autopilot.py
+++ autopilot.py
@@ -2,18 +2,30 @@ import logging
 import serial
 import time
 
+from dataclasses import dataclass
 from SimConnect import *
+from typing import Callable
 
-# Values as read from the wire
-heading = None
-altitude = None
-speed = None
-vspeed = None
-qnh = None
+@dataclass
+class Element:
+	name: str
+	id: str
+	var: str # Value as read from the serial wire
+	event: str
+	read_modifier: Callable[[int], int] = lambda x: x
+	update_modifier: Callable[[int], int] = lambda x: x
+	val: int = None
 
+elements = [
+	Element('Heading', 'H', 'AUTOPILOT_HEADING_LOCK_DIR', 'HEADING_BUG_SET'),
+	Element('Altitude', 'A', 'AUTOPILOT_ALTITUDE_LOCK_VAR', 'AP_ALT_VAR_SET_ENGLISH', lambda x: x/100, lambda x: x*100),
+	Element('Speed', 'S', 'AUTOPILOT_AIRSPEED_HOLD_VAR', 'AP_SPD_VAR_SET'),
+	Element('Vertical Speed', 'V', 'AUTOPILOT_VERTICAL_HOLD_VAR', 'AP_VS_VAR_SET_ENGLISH', lambda x: x/10, lambda x: x*10),
+	# MS's baro, which is only displayed in INHg, is of course in units of 1/16th of a millibar in the API
+	Element('QNH', 'Q', 'KOHLSMAN_SETTING_HG', 'KOHLSMAN_SET', lambda x: x*100, lambda x: round(((x / 100) * 33.864) * 16)),
+]
 
-
-
+# Setup
 def connect_sim(sc):
 	try:
 		sc.connect()
@@ -31,34 +43,12 @@ def connect_serial():
 		connect_serial()
 
 
+# Incoming from sim
 def read_state():
-	read_heading()
-	read_altitude()
-	read_speed()
-	read_vspeed()
-	read_qnh()
+	for e in elements:
+		e.val = read_val(e.var, e.id, e.val, e.read_modifier)
 
-def read_heading():
-	global heading
-	heading = read_val('AUTOPILOT_HEADING_LOCK_DIR', 'H', heading)
-
-def read_altitude():
-	global altitude
-	altitude = read_val('AUTOPILOT_ALTITUDE_LOCK_VAR', 'A', altitude, lambda x: x / 100)
-
-def read_speed():
-	global speed
-	speed = read_val('AUTOPILOT_AIRSPEED_HOLD_VAR', 'S', speed)
-
-def read_vspeed():
-	global vspeed
-	vspeed = read_val('AUTOPILOT_VERTICAL_HOLD_VAR', 'V', vspeed, lambda x: x / 10)
-
-def read_qnh():
-	global qnh
-	qnh = read_val('KOHLSMAN_SETTING_HG', 'Q', qnh, lambda x: x * 100)
-
-def read_val(var, id, val, modifier = lambda x: x):
+def read_val(var, id, val, modifier):
 	new_val = ar.get(var)
 	if new_val == None:
 		return val
@@ -69,39 +59,32 @@ def read_val(var, id, val, modifier = lambda x: x):
 		ser.write(f'#{id}#{new_val})\r\n'.encode())
 	return new_val
 
-def update_heading(data):
-	global heading
-	heading = update_val(data, heading, 'HEADING_BUG_SET')
 
-def update_altitude(data):
-	global altitude
-	altitude = update_val(data, altitude, 'AP_ALT_VAR_SET_ENGLISH', lambda x: x * 100)
+# Incoming from AP Panel
+def handle_data(data):
+	print(f'Received #{data}')
+	cmd = data[0]
+	success = False
+	for e in elements:
+		if cmd == e.id:
+			e.val = update_val(data, e.val, e.event, e.update_modifier)
+			success = True
+			break
 
-def update_speed(data):
-	global speed
-	speed = update_val(data, speed, 'AP_SPD_VAR_SET')
+	if not success:
+		print(f'Unknown command: #{cmd}')
 
-def update_vspeed(data):
-	global vspeed
-	vspeed = update_val(data, vspeed, 'AP_VS_VAR_SET_ENGLISH', lambda x: x * 10)
-
-def update_qnh(data):
-	global qnh
-	# MS's baro, which is only displayed in INHg, is of course in units of 1/16th of a millibar in the API
-	modifier = lambda x: round(((x / 100) * 33.864) * 16)
-	qnh = update_val(data, speed, 'KOHLSMAN_SET', modifier)
-
-def update_val(data, val, event, modifier = lambda x: x):
+def update_val(data, val, event, modifier):
 	new_val = int(data[1:])
 	print(f'Updating #{event} from #{val} to #{new_val}')
 	if (new_val != val):
 		sim_val = modifier(new_val)
-		print(f'sim_val: #{sim_val}')
 		e = ae.find(event)
 		e(sim_val)
 	return new_val
 
 
+# Main
 ser = serial.Serial(port='COM3', baudrate=115200, timeout=0.2)
 
 sc = SimConnect(auto_connect=False)
@@ -117,14 +100,6 @@ print("Connected to serial port")
 while 1:
 	data = ser.readline().decode().strip()
 	if (data != ''):
-		print(f'Received #{data}')
-		cmd = data[0]
-		if cmd == 'H': update_heading(data)
-		elif cmd == 'A': update_altitude(data)
-		elif cmd == 'S': update_speed(data)
-		elif cmd == 'V': update_vspeed(data)
-		elif cmd == 'Q': update_qnh(data)
-		else: print(f'Unknown command: #{cmd}')
-
+		handle_data(data)
 	else:
 		read_state()
