188eab8dbf8fd17e2c75e52d0ec3bd9ecf101917
Bernard Duggan <bernard@guarana.org>
2021-01-01 16:32:08 +1100

    Improve digits

--- AutopilotPanel.ino
+++ AutopilotPanel.ino
@@ -12,6 +12,7 @@ typedef struct Element {
   long min;
   long max;
   long step;
+  byte decimal_pos;
   byte wrap_flag;
   bool initialised;
   long disp_val;
@@ -24,19 +25,19 @@ typedef struct Element {
 bool connected = false;
 
 Element elements[] = {
-  // Name           ID   RE address               LED   Min   Max   Inc Wrap
-  {"QNH mmHg",      'Q', i2cEncoderMiniLib(0x20), 0x70, 2600, 3100, 1,  i2cEncoderMiniLib::WRAP_DISABLE},
-  {"Heading °",     'H', i2cEncoderMiniLib(0x23), 0x72, 0,    359,  1,  i2cEncoderMiniLib::WRAP_ENABLE},
-  {"Altitude ft",   'A', i2cEncoderMiniLib(0x24), 0x71, 0,    600,  1,  i2cEncoderMiniLib::WRAP_DISABLE},
-  {"Speed kn",      'S', i2cEncoderMiniLib(0x22), 0x73, 0,    1000, 1,  i2cEncoderMiniLib::WRAP_DISABLE},
-  {"VSpeed 10 fpm", 'V', i2cEncoderMiniLib(0x21), 0x74, -800, 600,  5,  i2cEncoderMiniLib::WRAP_DISABLE},
+  // Name           ID   RE address               LED   Min   Max   Inc Dec Wrap
+  {"QNH mmHg",      'Q', i2cEncoderMiniLib(0x20), 0x70, 2600, 3100, 1,  1,  i2cEncoderMiniLib::WRAP_DISABLE},
+  {"Heading °",     'H', i2cEncoderMiniLib(0x23), 0x72, 0,    359,  1,  0,  i2cEncoderMiniLib::WRAP_ENABLE},
+  {"Altitude ft",   'A', i2cEncoderMiniLib(0x24), 0x71, 0,    600,  1,  3,  i2cEncoderMiniLib::WRAP_DISABLE},
+  {"Speed kn",      'S', i2cEncoderMiniLib(0x22), 0x73, 0,    1000, 1,  0,  i2cEncoderMiniLib::WRAP_DISABLE},
+  {"VSpeed 10 fpm", 'V', i2cEncoderMiniLib(0x21), 0x74, -800, 600,  5,  0,  i2cEncoderMiniLib::WRAP_DISABLE},
 };
 
 void setup() {
   Wire.begin();
   Serial.begin(115200);
   
-  for (int i=0; i<ELEMENTS; i++) {
+  for (int i = 0; i < ELEMENTS; i++) {
     Element &e = elements[i];
     e.initialised = false;
     e.display.begin(e.display_address);
@@ -65,11 +66,9 @@ void connect() {
       Serial.write('C');
       Serial.flush();
       connected = true;
-
-      for (int i=0; i<ELEMENTS; i++) {
-        Element &e = elements[i];
-        e.display.print(e.disp_val);
-        e.display.writeDisplay();
+ 
+      for (int i = 0; i < ELEMENTS; i++) {
+        update_display(elements[i]);
       }
     }
   }
@@ -86,15 +85,14 @@ void setup_encoder(Element &e) {
 void read_updates() {
   while (Serial.available()) {
     byte b = Serial.read();
-    for (int i=0; i<ELEMENTS; i++) {
+    for (int i = 0; i < ELEMENTS; i++) {
       Element &e = elements[i];
       if (b == e.id) {
         long val = Serial.parseInt();
         e.disp_val = val;
         e.sent_val = val;
         e.encoder.writeCounter(val);
-        e.display.print(val);
-        e.display.writeDisplay();
+        update_display(e);
         e.initialised = true;
         break;
       }
@@ -103,18 +101,18 @@ void read_updates() {
 }
 
 void read_inputs() {
-  for (int i=0; i<ELEMENTS; i++) {
+  for (int i = 0; i < ELEMENTS; i++) {
     Element &e = elements[i];
-    if (!e.initialised)
+    if (!e.initialised) {
       continue;
-    
+    }
+
     long val = e.encoder.readCounterLong();
     if (val != e.disp_val) {
       e.disp_val = val;
-      e.display.print(val);
-      e.display.writeDisplay();
+      update_display(e);
     }
-    
+
     if (e.sent_val != val && Serial.availableForWrite() >= 7) {
       Serial.print(e.id);
       Serial.println(val);
@@ -122,4 +120,53 @@ void read_inputs() {
     }
   }
   Serial.flush();
+}
+
+int pos;
+void update_display(Element &e) {
+  int val = e.disp_val;
+  bool negative = false;
+  
+  if (val < 0) {
+    val = -val;
+    negative = true;
+  }
+  
+  pos = 4;
+
+  // Write the digits
+  for (; pos >= 0; dec_pos()) {
+    e.display.writeDigitNum(pos, val % 10, e.decimal_pos == pos);
+    
+    if ((val /= 10) == 0) {
+      break;
+    }
+  }
+
+  // Add leading 0s until we have a whole number
+  while (e.decimal_pos && (pos > e.decimal_pos)) {
+    dec_pos();
+    e.display.writeDigitNum(pos, 0, e.decimal_pos == pos);
+  }
+
+  // Add a -ve sign if required
+  if (pos >= 0 && negative) {
+    e.display.writeDigitRaw(dec_pos(), 0x40); // Minus sign
+  }
+
+  // Blank the rest of the display
+  while (pos >= 0) {
+    e.display.writeDigitRaw(dec_pos(), 0);
+  }
+  
+  e.display.writeDisplay();
+}
+
+int dec_pos() {
+  if (pos == 3) {
+    pos = 1;
+  } else {
+    pos--;
+  }
+  return pos;
 }
